13dcd9305fe8a74d643782164dfa2ab80fcd7a55
Mark <kasdasd@gasgas.ga>
2025-04-30 23:27:02 +0200

    I writed the BrowserRouter struct in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Registration from './Registration';
+import SignIn from './SignIn';
 
 function App() {
 
   return (
-  <div> Hello!</div> 
+    <BrowserRouter>
+      <Routes>
+        <Route path="/regisztracio" element={<Registration />} />
+        <Route path="/belepes" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 const root = ReactDOM.createRoot(document.getElementById('root'));
