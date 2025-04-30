3453ae0759d87398837a2176eb0dd17649dad065
Mark <kasdasd@gasgas.ga>
2025-04-30 23:22:49 +0200

    I created Sign and Registration initial UI file.

---
+++ frontend/src/Registration.js
@@ -0,0 +1,11 @@
+import { Row, Col, Button, Form, Container } from 'react-bootstrap/';
+
+function Registration() {
+    
+    return (
+        <Container>
+            <span>Itt lesz a Regisztrációs felület.</span>
+        </Container>
+    );
+}
+export default Registration;
---
+++ frontend/src/SignIn.js
@@ -0,0 +1,11 @@
+import { Row, Col, Button, Form, Container } from 'react-bootstrap/';
+
+function SignIn() {
+    
+    return (
+        <Container>
+            <span>Itt lesz a Bejelentkezési felület.</span>
+        </Container>
+    );
+}
+export default SignIn;
