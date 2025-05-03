1473140521f3e17fc9455f2f97d798f192c4fc00
Mark <kasdasd@gasgas.ga>
2025-05-03 18:25:21 +0200

    Created register endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require("../models/user");
 const { sha1, createAccessToken, createRefreshToken } = require("../utils/userUtils");
 const FIFTEEN_MINUTES = 15 * 60 * 1000;
 const ONE_DAY = 24 * 60 * 60 * 1000;
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
     const { username, password } = req.body;
@@ -48,6 +49,71 @@ const login = async (req, res) => {
     }
 };
 
+const register = async (req, res) => {
+  const {username, password, repassword} = req.body;
+  try {
+    if (
+      !username ||
+      !password ||
+      !repassword
+    ) {
+      throw Error("Töltse ki a kötelező mezőket!");
+    }
+    if (password !== repassword) {
+      throw Error("A két jelszó nem egyezik!");
+    }
+    let user = await User.findOne({
+      attributes: ["username"],
+      where: { username: username },
+    });
+    if (user) {
+      throw Error("Ez a felhasználónév már létezik!");
+    }
+    const response = await User.create(
+      {
+        username: username,
+        password: sha1(password)
+      },
+      {
+        fields: [
+          "username",
+          "password"
+        ],
+      }
+    );
+
+    if (response !== null) {
+      const token = createToken(response.get("id"));
+      res.cookie("user", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: FIFTEEN_MINUTES,
+      });
+
+      const refreshToken = createRefreshToken(response.get("id"));
+      res.cookie("refresh", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: ONE_DAY,
+      });
+
+      console.log(response.get("id"));
+      res.status(200).json({
+        message: "ok",
+      });
+      return;
+    }
+    throw Error("Valami hiba történt!");
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({
+      error: error.message,
+    });
+  }
+};
 module.exports = {
-    login
+    login,
+    register
 };
