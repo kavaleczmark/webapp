5e03145666aebb7e22700e6862e5adf9c7197c06
Mark <kasdasd@gasgas.ga>
2025-05-08 17:52:51 +0200

    requireAuth created

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,23 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const requireAuth = async (req, res, next) => {
+    const cookies = req.cookies;
+    if (!cookies || !cookies.user) {
+      return res.status(401).json({ error: "Authorization token required" });
+    }
+    try { 
+      const { id } = jwt.verify(cookies.user, process.env.SECRET);
+      req.user = await User.findOne({
+          where : {id : id}
+      });
+      if (!req.user) {
+        throw Error("User doesn't exits");
+      }
+      next();
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ error: "Request is not authorized" });
+    }
+};
+module.exports = requireAuth;
