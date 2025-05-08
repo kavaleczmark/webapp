186a6c97b7da6af884bf179c2a1800760141951f
Mark <kasdasd@gasgas.ga>
2025-05-08 21:21:25 +0200

    updated sql

--- sql/jegyzettomb.sql
+++ sql/jegyzettomb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1:3306
--- Létrehozás ideje: 2025. Máj 08. 21:03
+-- Létrehozás ideje: 2025. Máj 08. 21:21
 -- Kiszolgáló verziója: 9.1.0
 -- PHP verzió: 8.3.14
 
@@ -46,7 +46,8 @@ CREATE TABLE IF NOT EXISTS `noteshistory` (
   `version_id` int NOT NULL,
   `title` varchar(255) NOT NULL,
   `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `date` datetime NOT NULL
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`notes_id`,`version_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -63,14 +64,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `reg_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- A tábla adatainak kiíratása `users`
---
-
-INSERT INTO `users` (`id`, `username`, `password`, `reg_date`) VALUES
-(1, 'markteszt12', '2891baceeef1652ee698294da0e71ba78a2a4064', '2025-05-08 18:28:09'),
-(2, 'asd', '2891baceeef1652ee698294da0e71ba78a2a4064', '2025-05-08 18:31:27');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
