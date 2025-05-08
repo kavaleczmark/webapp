9c192dc60a3d185176103a3afcca31ef5dd348b4
Mark <kasdasd@gasgas.ga>
2025-05-08 21:03:42 +0200

    updated sql

--- sql/jegyzettomb.sql
+++ sql/jegyzettomb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1:3306
--- Létrehozás ideje: 2025. Máj 01. 21:42
+-- Létrehozás ideje: 2025. Máj 08. 21:03
 -- Kiszolgáló verziója: 9.1.0
 -- PHP verzió: 8.3.14
 
@@ -31,10 +31,22 @@ DROP TABLE IF EXISTS `notes`;
 CREATE TABLE IF NOT EXISTS `notes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
-  `date` datetime NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `text` text NOT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `noteshistory`
+--
+
+DROP TABLE IF EXISTS `noteshistory`;
+CREATE TABLE IF NOT EXISTS `noteshistory` (
+  `notes_id` int NOT NULL,
+  `version_id` int NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `date` datetime NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -50,10 +62,17 @@ CREATE TABLE IF NOT EXISTS `users` (
   `password` varchar(255) NOT NULL,
   `reg_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- A tábla adatainak kiíratása `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `reg_date`) VALUES
+(1, 'markteszt12', '2891baceeef1652ee698294da0e71ba78a2a4064', '2025-05-08 18:28:09'),
+(2, 'asd', '2891baceeef1652ee698294da0e71ba78a2a4064', '2025-05-08 18:31:27');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
