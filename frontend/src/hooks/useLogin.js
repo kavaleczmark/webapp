84aaacf322bfcf654776067278a27f21b53752e0
dandomate <71004585+dandomate@users.noreply.github.com>
2025-05-08 18:08:33 +0200

    uselogin updated

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { instance as axios } from '../api/axios';
 import { useAuthContext } from './useAuthContext';
 
