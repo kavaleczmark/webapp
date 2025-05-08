f9568da63812b32e9392862c98e2375d8d421afb
dandomate <71004585+dandomate@users.noreply.github.com>
2025-05-08 17:25:45 +0200

    useLogin created

--- frontend/src/hooks/useLogin.js
+++ frontend/src/hooks/useLogin.js
@@ -1,1 +1,39 @@
-//uselogin hook
+import { useEffect, useRef, useState } from 'react';
+import { instance as axios } from '../api/axios';
+import { useAuthContext } from './useAuthContext';
+
+
+export const useLogin = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const [isFinished, setIsFinished] = useState(null);
+    const {dispatch} = useAuthContext();
+
+    const login = async (userName, password) => {
+        setIsLoading(true);
+        setError(null);
+        setIsFinished(false);
+        axios.post("user/login", {
+            username : `${userName}`,
+            password : `${password}`
+        }).then(function(response){
+            if(response.status === 200) {
+                localStorage.setItem("user", JSON.stringify(response.data));
+                dispatch({
+                    type: "LOGIN",
+                    payload: response
+                })
+                setIsLoading(false);
+                setIsFinished(true);
+                console.log(response);
+            }
+        }).catch(function(error){
+            console.log(error.response);
+            setIsLoading(false);
+            setIsFinished(true);
+            setError(error.response.data.error);
+            
+        });
+    }
+    return {login,isLoading,isFinished,error};
+}
