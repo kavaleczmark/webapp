da1d791a563b32e7e22e59c20665417546fc87ad
dandomate <71004585+dandomate@users.noreply.github.com>
2025-05-08 17:22:52 +0200

    Update Login page

--- frontend/src/SignIn.js
+++ frontend/src/SignIn.js
@@ -1,26 +1,74 @@
-import { useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Container, Row, Col, Form, Button, Card, Alert } from 'react-bootstrap';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useLogin } from './hooks/useLogin';
+import { ToastContainer, toast, Bounce} from 'react-toastify';
+
 
 function SignIn() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  const [error, setError] = useState('');
+  const {login, error, isLoading, isFinished}= useLogin();
+  const navigate = useNavigate();
+  const loginButton = useRef(null);
+  const toastId = useRef(null);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!username || !password) {
-      setError('Minden mező kitöltése kötelező!');
-      return;
-    }
-    setError('');
-    console.log('Bejelentkezés:', { username, password });
+    toastId.current = toast.loading("Kérem várjon!");
+    await login(username, password);
   };
+
+  useEffect(()=>{
+    if(error) {
+            toast.update(toastId.current, {
+                render: `${error}`,
+                type: "error",
+                isLoading: false,
+                closeButton: true,
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                pauseOnFocusLoss: true
+            });
+            return;   
+    }
+        if(!error && isFinished) {
+            toast.update(toastId.current, {
+                render: "Sikeres bejelentkezés",
+                type: "success",
+                isLoading: false,
+                closeButton: true,
+                autoClose:500,
+                hideProgressBar: false,
+                closeOnClick: false,
+                pauseOnHover: true,
+                draggable: true,
+                pauseOnFocusLoss: true,
+                onClose : () => {navigate("/jegyzetek")}
+            });
+        }
+    }, [isFinished, error]);
 
   return (
     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+        <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+                transition={Bounce}
+            />
       <Row className="w-100 justify-content-center">
         <Col xs={12} md={6} lg={4}>
           <Card className="shadow p-4">
@@ -30,10 +78,11 @@ function SignIn() {
               <Form.Group controlId="formUsername" className="mb-3">
                 <Form.Label>Felhasználónév</Form.Label>
                 <Form.Control
+                  name="username"
                   type="text"
                   placeholder="Felhasználónév"
                   value={username}
-                  onChange={(e) => setUsername(e.target.value)}
+                  required onChange={(e) => setUsername(e.target.value)}
                 />
               </Form.Group>
 
@@ -41,10 +90,11 @@ function SignIn() {
                 <Form.Label>Jelszó</Form.Label>
                 <div className="d-flex">
                   <Form.Control
+                    name="password"
                     type={showPassword ? 'text' : 'password'}
                     placeholder="Jelszó"
                     value={password}
-                    onChange={(e) => setPassword(e.target.value)}
+                    required onChange={(e) => setPassword(e.target.value)}
                   />
                   <Button
                     variant="outline-secondary"
@@ -56,7 +106,7 @@ function SignIn() {
                 </div>
               </Form.Group>
 
-              <Button type="submit" className="w-100 mt-3 mb-2" variant="primary">
+              <Button ref={loginButton} disabled={isLoading} type="submit" className="w-100 mt-3 mb-2" variant="primary">
                 Bejelentkezés
               </Button>
             </Form>
