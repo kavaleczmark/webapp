bfb411c51ba4684e05d48546d88daca9895dafb0
Mark <kasdasd@gasgas.ga>
2025-05-11 21:02:17 +0200

    add new endpoint in router

--- backend/routes/notesHistory.js
+++ backend/routes/notesHistory.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const {
     getLatestNotesForUser,
-    createNote
+    createNote,
+    deleteNoteAndHistory
 } = require("../controllers/noteHistoryController");
 const requireAuth = require("../middleware/requireAuth");
 
 router.get("/getNotes",requireAuth, getLatestNotesForUser);
 router.post("/create",requireAuth, createNote);
+router.delete('/delete/:id', requireAuth, deleteNoteAndHistory);
 module.exports = router;
