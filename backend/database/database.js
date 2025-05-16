3d68ed5c713291890f7f009e0addb197869d9dd2
Mark <kasdasd@gasgas.ga>
2025-05-16 10:15:26 +0200

    created .env for render

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,38 +1,28 @@
 const Sequelize = require("sequelize");
+
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-        host: process.env.DB_HOST,
-        dialect: "mysql",
-        port: process.env.DB_PORT,
-        define: {
-            timestamps: false,
-            freezeTableName: true,
-        },
-        dialectOptions: {
-            useUTC: false, 
-            dateStrings: true,
-            typeCast: function (field, next) { 
-              if (field.type === 'DATETIME') {
-                return field.string()
-              }
-                return next()
-              },
-          },
-        timezone: '+02:00'
-    }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+    port: process.env.DB_PORT,
+    define: {
+      timestamps: false,
+      freezeTableName: true,
+    },
+    timezone: '+02:00'
+  }
 );
+
 sequelize
-    .sync({
-        force: false
-    })
-    .then(()=>{
-        console.log("Database connected!");
-    })
-    .catch((error)=>{
-        console.error("Failed to connect to database! ",error);
-    })
+  .sync({ force: false })
+  .then(() => {
+    console.log("Database connected!");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to database! ", error);
+  });
 
 module.exports = sequelize;
